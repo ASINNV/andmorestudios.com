66b08699dbececa510deffaa8447f83547aa43fb
ASINNV <omgitsadrian@gmail.com>
2016-05-05 18:52:47 -0700

    splash page text add

--- script.js
+++ script.js
@@ -2,10 +2,37 @@ $.fn.scrollBottom = function() {
   return $(document).height() - this.scrollTop() - this.height(); 
 };
 
-
+var leaveCounter = 0;
 $(document).ready(function() {
-	$( ".line" ).animate({width: "36%"}, 1400, function() {
-		$( ".enter-button" ).fadeIn( 1500 );
+	(function() {
+		var altName = $( ".alt-name" );
+		var nameIndex = -1;
+		function showNextName() {
+			++leaveCounter;
+			if (leaveCounter === 7) {
+				altName.eq((nameIndex % altName.length) + 1).fadeIn(300);
+				return 0;
+			}
+			++nameIndex;
+			altName.eq(nameIndex % altName.length)
+				.fadeIn(300)
+				.delay(300)
+				.fadeOut(300, showNextName);
+		}
+		
+		showNextName();
+
+		
+	})();
+	setTimeout(function() {
+		$( ".studios" ).animate({opacity: "1"}, 300);
+	}, 6300);
+	$( ".line" ).animate({width: "46%"}, 6600, function() {
+		setTimeout(function() {
+				
+			$( ".enter-button" ).fadeIn( 300 );	
+				
+		}, 600);
 	});
 	$( ".menu-button" ).click(function() {
 		$( ".menu" ).slideToggle( 500 );
