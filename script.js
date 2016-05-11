94d90a7576d71e1651c78fba0c73cbb98593ab2b
ASINNV <omgitsadrian@gmail.com>
2016-05-11 14:14:43 -0700

    Make landing page lead-in start after 900ms instead of 600ms

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 					
 			}, 600);
 		});
-	}, 600);
+	}, 900);
 	$( ".menu-button" ).click(function() {
 		$( ".menu" ).slideToggle( 500 );
 		$( ".clickCatcher" ).show();
